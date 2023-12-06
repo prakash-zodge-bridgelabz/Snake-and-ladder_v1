4a784119c9cdd4720aaa082a956926ab4d10eabc
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-06 17:09:05 +0530

    Modified: Player rolls the dice to get number between 1 to 6

--- src/com/bridgelabz/Snake_And_Ladder.java
+++ src/com/bridgelabz/Snake_And_Ladder.java
@@ -5,5 +5,9 @@ public class Snake_And_Ladder {
         System.out.println("Welcome to Snake and Ladder game");
         //Snake and Ladder game played with single player at start position 0
         int start = 0; //UC 1
+
+        //UC2
+        // Roll the dice
+        int diceRoll = (int) (Math.random() * 6) + 1;
     }
 }

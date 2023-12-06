c0063ae6087702df52edec61be0f25ffb1501e01
prakash-zodge-bridgelabz <prakash.zodge.bridgelabz@gmail.com>
2023-12-06 17:06:45 +0530

    Modified: Single player at start position 0

--- src/com/bridgelabz/Snake_And_Ladder.java
+++ src/com/bridgelabz/Snake_And_Ladder.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class Snake_And_Ladder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder game");
+        //Snake and Ladder game played with single player at start position 0
+        int start = 0; //UC 1
     }
 }
